f7b0e5f59531388aac882a664e5d439e3faec6df
Mark wachira <85750231+sudo-apt-get-mark@users.noreply.github.com>
2023-04-04 12:28:46 +0300

    Add files via upload

---
+++ cht/chatbot/static/scripts/chat.js
@@ -0,0 +1,101 @@
+// Collapsible
+var coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+
+    });
+}
+
+// chat-clock
+let myVar = setInterval(myTimer, 1000);
+function myTimer() {
+    const d = new Date();
+    document.getElementById("chat-clock").innerHTML = d.toLocaleTimeString();
+}
+
+// Gets the first message
+function firstBotMessage() {
+    let firstMessage = "Habari."
+    document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>'
+
+    document.getElementById("userInput").scrollIntoView(false);
+}
+
+firstBotMessage();
+
+// Retrieves the response
+function getHardResponse(userText) {
+    let botResponse = getBotResponse(userText);
+    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
+    $("#chatbox").append(botHtml);
+
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+}
+
+//Gets the text from the input box and processes it
+function getResponse() {
+    let userText = $("#textInput").val();
+
+    if (userText == "") {
+        userText = "Programmer Shah Jalal, all the best!";
+    }
+
+    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+    setTimeout(() => {
+        getHardResponse(userText);
+    }, 1000)
+
+}
+
+// Handles sending text via button clicks
+function buttonSendText(sampleText) {
+    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
+
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+    //Uncomment this if you want the bot to respond to this buttonSendText event
+    // setTimeout(() => {
+    //     getHardResponse(sampleText);
+    // }, 1000)
+}
+
+function sendButton() {
+    getResponse();
+}
+/* emoji script */
+const button = document.querySelector('#emoji-button');
+
+const picker = new EmojiButton();
+
+button.addEventListener('click', () => {
+    picker.togglePicker(button);
+
+});
+
+picker.on('emoji', emoji => {
+    document.querySelector('input').value += emoji;
+});
+
+// Press enter to send a message
+$("#textInput").keypress(function (e) {
+    if (e.which == 13) {
+        getResponse();
+    }
+});
---
+++ cht/chatbot/static/scripts/responses.js
@@ -0,0 +1,65 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "mambo") {
+        return "poa sana uko aje ";
+    } else if (input == "niko salama" , "niko poa") {
+        return "Shukran! Alhamdulillah and you?";
+    } else if (input == "am good") {
+        return "naeza ongea sheng, unadai?";
+    }
+    else if (input == "what is the current news") {
+        return "rao alikataa mandamano, huskii aliingizwa box";
+    }
+    else if (input == "Fine and you?") {
+        return "Alhamdulillah";
+    }
+    else if (input == "Fine and you?") {
+        return "Alhamdulillah";
+    }
+    else if (input == "Fine and you?") {
+        return "Alhamdulillah";
+    }
+
+    //greeting
+    if (input == "what is the current news") {
+        return "rao alikataa mandamano, huskii aliingizwa box";
+    } else if (input == "fine") {
+        return "Great, May Allah bless you!";
+    } else if (input == "hi") {
+        return "hello 👋";
+    }
+    //Alhamdulillah
+    if (input == "alhamdulillah") {
+        return "Alhamdulillah..! Allah is enough for us.";
+    } else if (input == "Alhamdulillah") {
+        return "Our life should be maintained only for praising of Allah (SWT).";
+    } else if (input == "who are you?") {
+        return "I'm a chatbot not a human.";
+    }
+    //remain
+    if (input == "Are you busy?") {
+        return "Ahoy dear! I am here only for chat with you.";
+    } else if (input == "Is there anyone available?") {
+        return "Thanks for asking me. Yes, I'm available 24 hours for you. How can I help you?";
+    } else if (input == "Where are you from?") {
+        return "I am from Bangladesh.";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there 👋!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else! Our team will contact you soon.";
+    } 
+
+    // Simple responses
+    if (input == "wozaa") {
+        return "wozaa wozaa";
+    } else if (input == "semaje") {
+        return "Si ubonge yooo";
+    } else {
+        return "Try asking something else! Our team will contact you soon.";
+    } 
+}
